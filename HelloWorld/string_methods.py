5132874d2f8ca867430704cbf1cfec2c88820c50
chai <49208740+ssumachai@users.noreply.github.com>
2022-06-21 12:31:36 -0700

    Learned String Methods (Simple)

---
+++ HelloWorld/string_methods.py
@@ -0,0 +1,16 @@
+course = 'Python for Beginners'
+print(len(course))
+
+idol = 'Im Nayeon'
+print(idol.upper())
+print(idol.lower())
+print(idol)
+
+maze = 'Chaeyoung'
+print(maze.find('C'))
+print(maze.find('z'))
+print(maze.find('young'))
+print(maze.replace('eyoung', 'i'))
+
+print('Chae' in maze)
+print('chae' in maze)
