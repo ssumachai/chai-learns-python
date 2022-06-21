e35ff6c805f4e3ff9b366b86bfc264fe08ac7a48
chai <49208740+ssumachai@users.noreply.github.com>
2022-06-21 12:48:11 -0700

    Math tings

---
+++ arithmetic_operators.py
@@ -0,0 +1,17 @@
+print(10 + 3)       # Addition
+print(10 - 3)       # Subtraction
+print(10 * 3)       # Multiplication
+print(10 / 3)       # Yields Floating Quotient
+print(10 // 3)      # Yields Int Quotient
+print(10 % 3)       # Modulo
+print(10 ** 3)      # Power
+
+x = 10
+x += 3              # Augmented Addition
+print(x)
+
+a = 10 + 3 * 2 ** 2 
+print(a)
+
+b = (2 + 3) * 10 - 3
+print(b)
